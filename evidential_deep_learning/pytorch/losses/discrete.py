ce7ed5d05d7149378c098e9e2911cd9c9c4ebd0d
Dariusrussellkish <russeldk@bc.edu>
2020-12-24 15:53:08 -0500

    Reimplement Dirichlet_SOS based on @dougbrion
    - from https://github.com/dougbrion/pytorch-classification-uncertainty
    - MIT license
    - Dirichlet dist. with log-loss
    - No KL divergence annealing

--- evidential_deep_learning/pytorch/losses/discrete.py
+++ evidential_deep_learning/pytorch/losses/discrete.py
@@ -1,38 +1,103 @@
 import torch
-import warnings
+import torch.nn.functional as F
 
 BCELoss = torch.nn.BCEWithLogitsLoss()
 
 
-def _KL_(alpha):
-    beta = torch.ones((1, alpha.shape[1]), dtype=torch.float)
-    S_alpha = alpha.sum(dim=1)
-    S_beta = beta.sum(dim=1)
-    lnB = torch.lgamma(S_alpha) - torch.lgamma(alpha).sum(dim=1)
-    lnB_uni = torch.lgamma(beta).sum(dim=1) - torch.lgamma(S_beta)
+def Dirichlet_SOS(y, outputs, device=None):
+    return edl_log_loss(outputs, y, device=device if device else outputs.device)
+
+
+def Dirichlet_Evidence(outputs):
+    """Calculate ReLU evidence"""
+    return relu_evidence(outputs)
+
+
+def Dirichlet_Matches(predictions, labels):
+    """Calculate the number of matches from index predictions"""
+    assert predictions.shape == labels.shape, f"Dimension mismatch between predictions " \
+                                              f"({predictions.shape}) and labels ({labels.shape})"
+    return torch.reshape(torch.eq(predictions, labels).float(), (-1, 1))
+
+
+def Dirichlet_Predictions(outputs):
+    """Calculate predictions from logits"""
+    return torch.argmax(outputs, dim=1)
+
+
+def Dirichlet_Uncertainty(outputs):
+    """Calculate uncertainty from logits"""
+    alpha = relu_evidence(outputs) + 1
+    return alpha.size(1) / torch.sum(alpha, dim=1, keepdim=True)
+
+
+def Sigmoid_CE(y, y_logits, device=None):
+    return BCELoss(y_logits, y, device=device if device else y_logits.device)
+
+
+# MIT License
+#
+# Copyright (c) 2019 Douglas Brion
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+def relu_evidence(y):
+    return F.relu(y)
+
+
+def exp_evidence(y):
+    return torch.exp(torch.clamp(y, -10, 10))
+
+
+def softplus_evidence(y):
+    return F.softplus(y)
+
+
+def kl_divergence(alpha, num_classes, device=None):
+    beta = torch.ones([1, num_classes], dtype=torch.float32, device=device)
+    S_alpha = torch.sum(alpha, dim=1, keepdim=True)
+    S_beta = torch.sum(beta, dim=1, keepdim=True)
+    lnB = torch.lgamma(S_alpha) - \
+          torch.sum(torch.lgamma(alpha), dim=1, keepdim=True)
+    lnB_uni = torch.sum(torch.lgamma(beta), dim=1,
+                        keepdim=True) - torch.lgamma(S_beta)
 
     dg0 = torch.digamma(S_alpha)
     dg1 = torch.digamma(alpha)
 
-    kl = ((alpha - beta) * (dg1 - dg0)).sum(dim=1) + lnB + lnB_uni
+    kl = torch.sum((alpha - beta) * (dg1 - dg0), dim=1,
+                   keepdim=True) + lnB + lnB_uni
     return kl
 
 
-def Dirichlet_SOS(y, alpha):
-    # TODO: Validate this port, too many reductions in tf to track
-    warnings.warn(f"This function is not validated")
-    S = alpha.sum(dim=1)
-    m = alpha / S
+def edl_loss(func, y, alpha, device=None):
+    S = torch.sum(alpha, dim=1, keepdim=True)
+    A = torch.sum(y * (func(S) - func(alpha)), dim=1, keepdim=True)
 
-    A = (y-m).square().sum(axis=1)
-    B = (alpha * (S-alpha)) / (S.square() * (S+1))
-    B = B.sum(axis=1)
-
-    alpha_hat = y + (1-y)*alpha
-    C = _KL_(alpha_hat)
-    C = C.mean(dim=1)
-    return (A + B + C).mean()
+    kl_alpha = (alpha - 1) * (1 - y) + 1
+    kl_div = kl_divergence(kl_alpha, y.shape[1], device=device)
+    return A + kl_div
 
 
-def Sigmoid_CE(y, y_logits):
-    return BCELoss(y_logits, y)
+def edl_log_loss(output, target, device=None):
+    evidence = relu_evidence(output)
+    alpha = evidence + 1
+    loss = torch.mean(edl_loss(torch.log, target, alpha, device=device))
+    assert loss is not None
+    return loss
