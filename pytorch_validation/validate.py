ff58a90f48b70c045a3878d5a1963da8282d6a05
Dariusrussellkish <russeldk@bc.edu>
2020-12-24 16:45:49 -0500

    added license attribution

--- pytorch_validation/validate.py
+++ pytorch_validation/validate.py
@@ -5,7 +5,6 @@ from evidential_deep_learning.pytorch.losses import *
 import pytorch_lightning as pl
 from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.callbacks.lr_monitor import LearningRateMonitor
-from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 import matplotlib.pyplot as plt
 import scipy.ndimage as nd
 from torchvision.datasets.mnist import MNIST
@@ -22,6 +21,28 @@ if __name__ == '__main__':
 
     model.eval()
     model.freeze()
+
+    # MIT License
+    #
+    # Copyright (c) 2019 Douglas Brion
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy
+    # of this software and associated documentation files (the "Software"), to deal
+    # in the Software without restriction, including without limitation the rights
+    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    # copies of the Software, and to permit persons to whom the Software is
+    # furnished to do so, subject to the following conditions:
+    #
+    # The above copyright notice and this permission notice shall be included in all
+    # copies or substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
 
     num_classes = 10
     Mdeg = 180
