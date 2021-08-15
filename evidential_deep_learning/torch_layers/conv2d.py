a7e4c32f9040b53bd83c10957d9c5e97d2574a79
Kelvinthedrugger <890225kelvin@gmail.com>
2021-08-16 03:24:49 +0800

    shape problem remained in layers

--- evidential_deep_learning/torch_layers/conv2d.py
+++ evidential_deep_learning/torch_layers/conv2d.py
@@ -3,8 +3,32 @@ import torch.nn as nn
 
 
 class Conv2DNormal(nn.Module):
-    pass
+    def __init__(self, filters, kernel_size, **kwargs):
+        self.filters = filters
+        self.kernel_size = kernel_size
+        super(Conv2DNormal, self).__init__()
+        self.conv2d = torch.nn.Conv2d(kernel_size=kernel_size)
+
+    def call(self, x):
+        pass
+
+    def compute_output_shape(self, input_shape):
+        pass
+
+    # search for get config in torch
 
 
 class Conv2DNormalGamma(nn.Module):
-    pass
+    def __init__(self, filters, kernel_size, **kwargs):
+        pass
+
+    def evidence(self, x):
+        pass
+
+    def call(self, x):
+        pass
+
+    def compute_output_shape(self, input_shape):
+        pass
+
+    # search for get config in torch
--- evidential_deep_learning/torch_layers/dense.py
+++ evidential_deep_learning/torch_layers/dense.py
@@ -3,16 +3,59 @@ import torch.nn as nn
 
 
 class DenseNormal(nn.Module):
-    pass
+    def __init__(self, units):
+        super(DenseNormal, self).__init__()
+        self.units = units  # check type
+        self.dense = '''TODO'''
+
+    def call(self, x):
+        pass
+
+    def compute_output_shape(self, input_shape):
+        pass
+
+    # search for get config in torch
 
 
 class DenseNormalGamma(nn.Module):
-    pass
+    def __init__(self, units):
+        super(DenseNormalGamma, self).__init__()
+        self.units = units  # check type
+        self.dense = '''TODO'''
+
+    def evidence(self, x):
+        pass
+
+    def call(self, x):
+        pass
+
+    def compute_output_shape(self, input_shape):
+        pass
+
+    # get config
 
 
 class DenseDirichlet(nn.Module):
-    pass
+    def __init__(self, units):
+        super(DenseDirichlet, self).__init__()
+        self.units = units  # check type
+        self.dense = '''TODO'''
+
+    def call(self, x):
+        pass
+
+    def compute_output_shape(self, input_shape):
+        pass
 
 
 class DenseSigmoid(nn.Module):
-    pass
+    def __init__(self, units):
+        super(DenseSigmoid, self).__init__()
+        self.units = units  # check type
+        self.dense = '''TODO'''
+
+    def call(self, x):
+        pass
+
+    def compute_output_shape(self, input_shape):
+        pass
